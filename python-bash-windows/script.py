f7310c70a60377ba927836bcb73acca938354e3c
Edoardo Vacchi <evacchi@users.noreply.github.com>
2023-01-26 09:03:46 +0100

    test: python-bash-windows

---
+++ python-bash-windows/script.py
@@ -0,0 +1,25 @@
+import subprocess
+
+args = [
+    'bash',
+    'script.sh',
+]
+
+result = subprocess.run(
+    args,
+    capture_output=True,
+    text=True,
+    check=False,
+)
+
+print(result)
+
+if result.returncode != 0:
+    print("STDOUT:") 
+    print(result.stdout)
+    print("--") 
+    print("STDERR:") 
+    print(result.err)
+    print("--") 
+
+exit(result.returncode)
